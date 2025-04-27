5447dcc1329e8d0f7246042975e85ae54fb351bb
HRGopher <keplerplanetgame@gmail.com>
2025-04-27 16:49:47 +0200

    done it was pretty easy imo

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const print = std.debug.print;
+const bytesToHex = std.fmt.bytesToHex;
 
+const RGB = struct { r: u8, g: u8, b: u8 };
+
+fn rgbToHex(rgb: RGB) void {
+    const rgb_bytes = [3]u8{ rgb.r, rgb.g, rgb.b };
+    const hex_color = bytesToHex(rgb_bytes, .lower);
+    print("#{s}", .{hex_color});
+}
 pub fn main() !void {
-    print("All your {s} are belong to us.\n", .{"codebase"});
+    rgbToHex(RGB{ .r = 173, .g = 216, .b = 230 });
 }
